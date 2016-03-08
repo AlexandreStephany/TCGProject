97d7b8bc430d08a50871b326cb62aa299bbbb25a
AlexandreStephany <alex.stephany.1@gmail.com>
2016-03-08 10:01:32 +0100

    Working on the exchange algorithm

--- TcgTournament/TcgTournament/Models/Match.cs
+++ TcgTournament/TcgTournament/Models/Match.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -33,6 +34,16 @@ namespace TcgTournament.Models
             }
             
         }
+        public bool PlayerOnThisMatch(Player player)
+        {
+            if (this.Result.ContainsKey(player))
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
 
         public Player GetLoser()
         {
@@ -49,6 +60,25 @@ namespace TcgTournament.Models
                 return secondPlayer.Key;
             }
 
+        }
+
+        internal int getPlayerIndex(Player player)
+        {
+            Player[] matchPlayers = new Player[2];
+            this.Result.Keys.CopyTo(matchPlayers,0);
+            if (player.Equals(matchPlayers[0]))
+            {
+                return 0;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+
+        internal void ChangePlayer(Player secondP, Player firstP)
+        {
+            throw new NotImplementedException();
         }
     }
 
--- TcgTournament/TcgTournament/Models/Tournament.cs
+++ TcgTournament/TcgTournament/Models/Tournament.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Timers;
+using System.Media;
 
 namespace TcgTournament.Models
 {
@@ -16,27 +17,62 @@ namespace TcgTournament.Models
         {
             participating = new List<Player>();
             matchesByRound = new Dictionary<int, List<Match>>();
+            this.Chrono = new Timer();
+            this.Chrono.Elapsed += ActivateAlarm;
+        }
+        public Tournament(List<Player> players, int timeLaps)
+        {
+            participating = players;
+            matchesByRound = new Dictionary<int, List<Match>>();
+            chrono = new Timer(timeLaps);
+            this.Chrono.Elapsed += ActivateAlarm;
+
         }
 
         internal void PermutatePlayers(Player firstP, Player secondP)
         {
             List<Match> CurrentMatches = new List<Match>();
             this.matchesByRound.TryGetValue(this.matchesByRound.Count - 1, out CurrentMatches);
+            bool permutationMade = false;
+            for(int i = 0; i < CurrentMatches.Count && !permutationMade ; i++)
+            {
+                Match firstMatch = CurrentMatches[i];
+                if(firstMatch.PlayerOnThisMatch(firstP))
+                {
+                    
+                    for (int j = 0; j < CurrentMatches.Count && !permutationMade; j++)
+                    {
+                        Match secondMatch = CurrentMatches[j];
+                        if (secondMatch.PlayerOnThisMatch(secondP))
+                        {
+
+                            firstMatch.ChangePlayer(firstP, secondP);
+                            secondMatch.ChangePlayer(secondP,firstP);
+                        }
+
+                    }
+
+                    }else if(firstMatch.PlayerOnThisMatch(secondP)){
+
+                }
+            }
             
         }
+        
 
         internal void StartCountdown()
         {
+            
             this.chrono.Start();
         }
 
-        public Tournament(List<Player> players,Timer timer)
+        private static void ActivateAlarm(Object source, ElapsedEventArgs e)
         {
-            participating = players;
-            matchesByRound = new Dictionary<int, List<Match>>();
-            chrono = timer;
-            
+            SoundPlayer player = new SoundPlayer(@"..\Alarm.wav");
+            player.Play();
         }
+
+        
         public Timer Chrono
         {
             get { return this.chrono; }
