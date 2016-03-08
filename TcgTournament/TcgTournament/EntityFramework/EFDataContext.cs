bfad585cbb1ac581ebf1fb1b667a590a6e3324a6
AlexandreStephany <alex.stephany.1@gmail.com>
2016-03-08 15:41:56 +0100

    Start of the EF Part

---
+++ TcgTournament/TcgTournament/EntityFramework/EFDataContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TcgTournament.EntityFramework;
+using TcgTournament.EntityFramework.EfModels;
+
+namespace TcgTournament.EntityFramework
+{
+    public class EFDataContext: DbContext
+    {
+        public EFDataContext(string connexionString) : base(connexionString) { }
+
+        public DbSet<Match>Matches;
+        public DbSet<Player> Players;
+        public DbSet<Tournament> Tournaments;
+
+    }
+}
